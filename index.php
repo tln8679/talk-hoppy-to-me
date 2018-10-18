24f98fe8929598b816e3a9c5648e2b0240b17ac6
Taylor <tlnoble94@gmail.com>
2018-10-18 16:04:59 -0400

    login seperated from index

--- index.php
+++ index.php
@@ -43,28 +43,5 @@
 </div>
 
 <?php
-            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                echo '<div class="alert alert-warning" role="alert"><p> Sorry!</p>
-		 <p class="text-danger">No database created, yet.</p></div>';
-            }    
-        ?>
-<div class="form-group">
-    <form method="POST" action="index.php">
-        <p>
-            <label for="email">Email</label>
-            <span style="color:red">*</span>
-            <input id="email" style="width:250px" type="text" name="email" placeholder="Enter email" class="form-control"
-                required>
-        </p>
-        <p>
-            <label for="password">Password</label>
-            <span style="color:red">*</span>
-            <input type="password" id="password" style="width:250px" name="password" placeholder="Enter password" class="form-control"
-                required>
-        </p>
-        <input type="submit" name="submit" value="Login" class="btn btn-primary">
-    </form>
-</div>
-<?php
     include('includes/footer.html');
 ?>
---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+    $page_title = 'Beer, yum!';
+	// Include header html here
+    include('includes/header.html');
+?>
+
+<?php
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                echo '<div class="alert alert-warning" role="alert"><p> Sorry!</p>
+		 <p class="text-danger">No database created, yet.</p></div>';
+            }    
+        ?>
+<div class="form-group">
+    <form method="POST" action="index.php">
+        <fieldset>
+            <legend>
+                Welcome back!
+            </legend>
+        <p>
+            <label for="email">Email</label>
+            <span style="color:red">*</span>
+            <input id="email" style="width:250px" type="text" name="email" placeholder="Enter email" class="form-control"
+                required>
+        </p>
+        <p>
+            <label for="password">Password</label>
+            <span style="color:red">*</span>
+            <input type="password" id="password" style="width:250px" name="password" placeholder="Enter password" class="form-control"
+                required>
+        </p>
+        <input type="submit" name="submit" value="Login" class="btn btn-primary">
+        </fieldset>
+    </form>
+</div>
+<?php
+    include('includes/footer.html');
+?>
