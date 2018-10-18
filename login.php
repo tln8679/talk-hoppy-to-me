cfce2ca198be75751c3b657b956cffab328d50aa
Taylor <tlnoble94@gmail.com>
2018-10-18 16:17:29 -0400

    seperated login from the index

--- login.php
+++ login.php
@@ -10,25 +10,29 @@
 		 <p class="text-danger">No database created, yet.</p></div>';
             }    
         ?>
+
 <div class="form-group">
     <form method="POST" action="index.php">
+
         <fieldset>
             <legend>
                 Welcome back!
             </legend>
-        <p>
-            <label for="email">Email</label>
-            <span style="color:red">*</span>
-            <input id="email" style="width:250px" type="text" name="email" placeholder="Enter email" class="form-control"
-                required>
-        </p>
-        <p>
-            <label for="password">Password</label>
-            <span style="color:red">*</span>
-            <input type="password" id="password" style="width:250px" name="password" placeholder="Enter password" class="form-control"
-                required>
-        </p>
-        <input type="submit" name="submit" value="Login" class="btn btn-primary">
+            <div class="row justify-content-center col-lg-6 offset-lg-3">
+                <p>
+                    <label for="email">Email</label>
+                    <span style="color:red">*</span>
+                    <input id="email" style="width:250px" type="text" name="email" placeholder="Enter email" class="form-control"
+                        required>
+                </p>
+                <p>
+                    <label for="password">Password</label>
+                    <span style="color:red">*</span>
+                    <input type="password" id="password" style="width:250px" name="password" placeholder="Enter password"
+                        class="form-control" required>
+                </p>
+                <input type="submit" name="submit" value="Login" class="btn btn-primary">
+            </div>
         </fieldset>
     </form>
 </div>
