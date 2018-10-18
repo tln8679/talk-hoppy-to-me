f813f05b3eee7b66a1d7488e2ac2b375567377c7
Taylor <tlnoble94@gmail.com>
2018-10-18 15:59:39 -0400

    Form styling and made registration recursive

--- registration.php
+++ registration.php
@@ -2,88 +2,102 @@
 	$page_title = 'Register!';
     include('includes/header.html');
 ?>
-<div class="form-style">
-	<form method="POST" action="register.php">
-		<fieldset>
-			<legend>
-				<h2>Thanks for joining!</h2>
-			</legend>
-			<label>
-				<span>Basics</span>
-			</label>
+
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $name = $_REQUEST['fname'] . ' ' . $_REQUEST['lname'];
+        echo "<div class=\"alert alert-success\" role=\"alert\">
+		<p>Thanks for registering <strong>$name</strong></p>
+		</div>";
+		exit;
+    }    
+?>
+
+<form method="POST" action="registration.php">
+	<fieldset>
+		<legend>
+			<h2>Thanks for joining!</h2>
+		</legend>
+		<label>
+			<h4>Basics</h4>
+		</label>
+		<div class="form-group">
 			<br>
 			<p>
-				<label for="fname"></label>
-				<span class="required">*</span>
-				<input type="text" id="fname" name="fname" placeholder="First name" class="input-field" required>
+				<label for="fname">First name</label>
+				<span style="color:red">*</span>
+				<input type="text" id="fname" name="fname" placeholder="First name" class="form-control" required>
 			</p>
 			<p>
-				<label for="lname"></label>
-				<span class="required">*</span>
-				<input type="text" id="lname" name="lname" placeholder="Last name" class="input-field" required>
+				<label for="lname">Last name</label>
+				<span style="color:red">*</span>
+				<input type="text" id="lname" name="lname" placeholder="Last name" class="form-control" required>
 			</p>
 			<p>
-				<label for="email"></label>
-				<span class="required">*</span>
-				<input id="email" style="width:250px" type="text" name="email" placeholder="Email address" class="input-field"
+				<label for="email">Email</label>
+				<span style="color:red">*</span>
+				<input id="email" style="width:250px" type="text" name="email" placeholder="Email address" class="form-control"
 				 required>
 			</p>
 			<p>
-				<label for="password"></label>
-				<span class="required">*</span>
-				<input type="password" id="password" style="width:250px" name="password" placeholder="New password" class="input-field"
+				<label for="password">Password</label>
+				<span style="color:red">*</span>
+				<input type="password" id="password" style="width:250px" name="password" placeholder="New password" class="form-control"
 				 required>
 			</p>
 			<p>
-				<label for="confirm-password"></label>
-				<span class="required">*</span>
+				<label for="confirm-password">Re-enter password</label>
+				<span style="color:red">*</span>
 				<input type="password" id="confirm-password" style="width:250px" name="confirm-password" placeholder="Confirm password"
-				 class="input-field" required>
+				 class="form-control" required>
 			</p>
-			<br>
-			<label>
-				<span>Address</span>
-			</label>
+		</div>
+		<br>
+		<label>
+			<h4>Address</h4>
+		</label>
+		<div class="form-group">
 			<br>
 			<p>
-				<label for="street"></label>
-				<span class="required">*</span>
-				<input type="text" id="street" name="street" placeholder="Street" class="input-field" required>
+				<label for="street">Street</label>
+				<span style="color:red">*</span>
+				<input type="text" id="street" name="street" placeholder="Street" class="form-control" required>
 			</p>
 			<p>
-				<label for="city"></label>
-				<span class="required">*</span>
-				<input type="text" id="city" name="city" placeholder="City" class="input-field" required>
+				<label for="city">City</label>
+				<span style="color:red">*</span>
+				<input type="text" id="city" name="city" placeholder="City" class="form-control" required>
 			</p>
 			<p>
 
-				<label for="state"></label>
-				<span class="required">*</span>
-				<input type="text" id="state" name="state" placeholder="State" class="input-field" required>
+				<label for="state">State</label>
+				<span style="color:red">*</span>
+				<input type="text" id="state" name="state" placeholder="State" class="form-control" required>
 			</p>
 			<p>
-				<label for="zip"></label>
-				<span class="required">*</span>
-				<input type="text" id="zip" name="zip" placeholder="Zip" class="input-field" required>
+				<label for="zip">Zip code</label>
+				<span style="color:red">*</span>
+				<input type="text" id="zip" name="zip" placeholder="Zip" class="form-control" required>
 			</p>
+		</div>
+		<div class="form-group">
 			<br>
 			<p>
-				<label>
-					<span>Phone number</span>
+				<label for="phone">
+					<h4>Phone number</h4>
 				</label>
-				<br>
-				<label for="phone"></label>
-				<input type="text" class="input-field" name="phone" placeholder="910-999-9999" maxlength="12" />
+				<input type="text" class="form-control" name="phone" placeholder="910-999-9999" maxlength="12" />
 				<select id="phonetype" name="phonetype">
-					<option value="mobile" class="input-field">Mobile</option>
-					<option value="home" class="input-field">Home</option>
-					<option value="office" class="input-field">Office</option>
+					<option value="mobile" class="form-control">Mobile</option>
+					<option value="home" class="form-control">Home</option>
+					<option value="office" class="form-control">Office</option>
 				</select>
 			</p>
+		</div>
+		<div class="form-group">
 			<br>
-
 			<label>
-				<span>Preferred method of contact</span>
+				<h4>Preferred method of contact</h4>
 			</label><br>
 			<p>
 				<label for="contact">
@@ -98,16 +112,17 @@
 					<input type="radio" name="contact" value="postal" id="postalr"> Postal
 				</label>
 			</p>
-			<label for="terms">
-				<span>Agree to terms</span>
-			</label>
-			<input type="checkbox" id="terms" name="terms" class="input-field" required>
+		</div>
+		<label for="terms">
+			<h5 style="color:red">Agree to terms</h5>
+		</label>
+		<input type="checkbox" id="terms" name="terms" required>
 
-			<p>
-				<input type="submit" name="submit" value="Register">
-			</p>
-		</fieldset>
-	</form>
+		<p>
+			<input type="submit" name="submit" value="Register" class="btn btn-primary">
+		</p>
+	</fieldset>
+</form>
 </div>
 <?php
     include('includes/footer.html');
