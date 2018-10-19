8d1d08edc508693051a10762a0952ba3e27e59de
Taylor <tlnoble94@gmail.com>
2018-10-19 19:51:09 -0400

    finished feed ui

---
+++ feed.php
@@ -0,0 +1,41 @@
+<?php
+    $page_title = 'You, yum!';
+	// Include header html here
+    include('includes/header.html');
+?>
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Jane Doe</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Miller Lite</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>October 2018</span></h6>
+          <p>Solid American Brew.</p>
+          <hr>
+        </div>
+    </div>
+
+      <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Jacob Thomas</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>New Glarus Spotted Cow</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>July 2018</h6>
+          <p>Amazing</p>
+          <hr>
+        </div>
+      </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+<?php
+    include('includes/footer.html');
+?>
