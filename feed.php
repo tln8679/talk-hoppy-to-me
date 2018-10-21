9621713fe01fac17c7c246057f99d46e281b20c5
Taylor <tlnoble94@gmail.com>
2018-10-21 19:32:55 -0400

    design

--- feed.php
+++ feed.php
@@ -12,8 +12,8 @@
         <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-xxlarge w3-text-blue"></i>Jane Doe</h2>
         <div class="w3-container">
           <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw "></i>October 2018</span></h6>
-          <h6><b><span class="w3-opacity">Global rating: </span><span class="w3-text-amber">6/10</span></b></h6>
-          <h6><b><span class="w3-opacity">My rating: </span><span class="w3-text-indigo">6/10</span></b></h6>
+          <h5><b><span class="w3-opacity">Global rating: </span><span class="w3-text-amber">6/10</span></b></h5>
+          <h5><b><span class="w3-opacity">My rating: </span><span class="w3-text-indigo">6/10</span></b></h5>
           <h5 class="w3-opacity"><b>Name: Miller Lite</b></h5>
           <h5 class="w3-opacity"><b>Brewer: </b></h5>
           <p>Solid American Brew. One of my favorites of all time!</p>
@@ -25,8 +25,8 @@
         <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-xxlarge w3-text-blue"></i>Jacob Thomas</h2>
         <div class="w3-container">
           <h6 class="w3-text-blue"><i class="fa fa-calendar "></i>July 2018</h6>
-          <h6><b><span class="w3-opacity">Global rating: </span><span class="w3-text-amber">6/10</span></b></h6>
-          <h6><b><span class="w3-opacity">My rating: </span><span class="w3-text-indigo">6/10</span></b></h6>
+          <h5><b><span class="w3-opacity">Global rating: </span><span class="w3-text-amber">6/10</span></b></h5>
+          <h5><b><span class="w3-opacity">My rating: </span><span class="w3-text-indigo">6/10</span></b></h5>
           <h5 class="w3-opacity"><b>Name: New Glarus Spotted Cow</b></h5>
           <h5 class="w3-opacity"><b>Brewer: </b></h5>
           <p>Amazing beer! Will get again!</p>
--- profile.php
+++ profile.php
@@ -20,30 +20,30 @@
         </div>
         <div class="w3-container">
           <h2>Taylor Noble</h2>
-          <p><i class="fa fa-briefcase fa-fw  w3-large w3-text-blue"></i>Day drinker</p>
-          <p><i class="fa fa-home fa-fw  w3-large w3-text-blue"></i>Wilmington, NC</p>
-          <p><i class="fa fa-envelope fa-fw  w3-large w3-text-blue"></i>tln8679@uncw.edu</p>
-          <p><i class="fa fa-phone fa-fw  w3-large w3-text-blue"></i>1910435534</p>
+          <p><i class="fa fa-briefcase fa-fw  w3-large w3-text-indigo"></i>Day drinker</p>
+          <p><i class="fa fa-home fa-fw  w3-large w3-text-indigo"></i>Wilmington, NC</p>
+          <p><i class="fa fa-envelope fa-fw  w3-large w3-text-indigo"></i>tln8679@uncw.edu</p>
+          <p><i class="fa fa-phone fa-fw  w3-large w3-text-indigo"></i>1910435534</p>
           <hr>
 
-          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw  w3-text-blue"></i>Favorite Styles</b></p>
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw  w3-text-indigo"></i>Favorite Styles</b></p>
           <p>Stouts</p>
           <div class="w3-light-grey w3-round-xlarge w3-small">
-            <div class="w3-container w3-center w3-round-xlarge w3-blue" style="width:60%">60%</div>
+            <div class="w3-container w3-center w3-round-xlarge w3-indigo" style="width:60%">60%</div>
           </div>
           <p>American Pale Ales</p>
           <div class="w3-light-grey w3-round-xlarge w3-small">
-            <div class="w3-container w3-center w3-round-xlarge w3-blue" style="width:25%">
+            <div class="w3-container w3-center w3-round-xlarge w3-indigo" style="width:25%">
               <div class="w3-center w3-text-white">25%</div>
             </div>
           </div>
           <p>Kolsch</p>
           <div class="w3-light-grey w3-round-xlarge w3-small">
-            <div class="w3-container w3-center w3-round-xlarge w3-blue" style="width:10%">10%</div>
+            <div class="w3-container w3-center w3-round-xlarge w3-indigo" style="width:10%">10%</div>
           </div>
           <p>Sours</p>
           <div class="w3-light-grey w3-round-xlarge w3-small">
-            <div class="w3-container w3-center w3-round-xlarge w3-blue" style="width:5%">5%</div>
+            <div class="w3-container w3-center w3-round-xlarge w3-indigo" style="width:5%">5%</div>
           </div>
         </div><br>
       </div>
@@ -56,9 +56,9 @@
     <div class="w3-twothird">
 
       <div class="w3-container w3-card w3-white w3-margin-bottom">
-        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw  w3-xxlarge w3-text-blue"></i>Beers</h2>
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw  w3-xxlarge w3-text-indigo"></i>Beers</h2>
         <div class="w3-container">
-          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw "></i>October 2018</span></h6>
+          <h6 class="w3-text-indigo"><i class="fa fa-calendar fa-fw "></i>October 2018</span></h6>
           <h5 class="w3-opacity"><b>Miller Lite by $beer->get_maker();</b></h5>
           <p><b><span class="w3-opacity">Global rating: </span><span class="w3-text-amber">6/10</span></b></p>
           <p><b><span class="w3-opacity">I rate this a: </span><span class="w3-text-indigo">7/10</span></b></p>
@@ -66,7 +66,7 @@
           <hr>
         </div>
         <div class="w3-container">
-          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw "></i>Mar 2012</h6>
+          <h6 class="w3-text-indigo"><i class="fa fa-calendar fa-fw "></i>Mar 2012</h6>
           <h5 class="w3-opacity"><b>Michelob Ultra by $beer->get_maker();</b></h5>
           <p><b><span class="w3-opacity">Global rating: </span><span class="w3-text-amber">8/10</span></b></p>
           <p><b><span class="w3-opacity">I rate this a: </span><span class="w3-text-indigo">7/10</span></b></p>
@@ -74,7 +74,7 @@
           <hr>
         </div>
         <div class="w3-container">
-          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw "></i>July 2018</h6>
+          <h6 class="w3-text-indigo"><i class="fa fa-calendar fa-fw "></i>July 2018</h6>
           <h5 class="w3-opacity"><b>Spotted Cow by New Glarus</b></h5>
           <p><b><span class="w3-opacity">Global rating: </span><span class="w3-text-amber">10/10</span></b></p>
           <p><b><span class="w3-opacity">I rate this a: </span><span class="w3-text-indigo">10/10</span></b></p>
@@ -83,9 +83,9 @@
       </div>
 
       <div class="w3-container w3-card w3-white">
-        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw  w3-xxlarge w3-text-blue"></i>Favorites</h2>
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw  w3-xxlarge w3-text-indigo"></i>Favorites</h2>
         <div class="w3-container">
-          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw "></i>July 2018</h6>
+          <h6 class="w3-text-indigo"><i class="fa fa-calendar fa-fw "></i>July 2018</h6>
           <h5 class="w3-opacity"><b>Spotted Cow by New Glarus</b></h5>
           <p><b><span class="w3-opacity">Global rating: </span><span class="w3-text-amber">10/10</span></b></p>
           <p><b><span class="w3-opacity">I rate this a: </span><span class="w3-text-indigo">10/10</span></b></p>
@@ -93,7 +93,7 @@
           <hr>
         </div>
         <div class="w3-container">
-          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw "></i>October 2013</h6>
+          <h6 class="w3-text-indigo"><i class="fa fa-calendar fa-fw "></i>October 2013</h6>
           <h5 class="w3-opacity"><b>Busch by $beer->get_maker();</b></h5>
           <p><b><span class="w3-opacity">Global rating: </span><span class="w3-text-amber">4/10</span></b></p>
           <p><b><span class="w3-opacity">I rate this a: </span><span class="w3-text-indigo">1/10</span></b></p>
@@ -101,7 +101,7 @@
           <hr>
         </div>
         <div class="w3-container">
-          <h6 class="w3-text-blue"><i class="fa fa-calendar fa-fw "></i>Birth</h6>
+          <h6 class="w3-text-indigo"><i class="fa fa-calendar fa-fw "></i>Birth</h6>
           <h5 class="w3-opacity"><b>Milwaukee's Best by $beer->get_maker();</b></h5>
           <p><b><span class="w3-opacity">Global rating: </span><span class="w3-text-amber">6/10</span></b></p>
           <p><b><span class="w3-opacity">I rate this a: </span><span class="w3-text-indigo">6/10</span></b></p>
