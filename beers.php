a6ca3ca155ca5af58f93b9a8f91312ecd6e0909a
Taylor <tlnoble94@gmail.com>
2018-10-21 18:31:42 -0400

    beers.php now takes in an array of objects

--- beers.php
+++ beers.php
@@ -2,17 +2,28 @@
     $page_title = 'You, yum!';
 	// Include header html here
     include('includes/header.html');
-?>
+    // You need this because the include in beer_data is now processed from this dir
+    include("beans/beer.php");
+    // getting the Array for populating the cards
+    include("tests/beer_data.php");
 
-<?php
     // Instantiating an array that we will use to populate the cards
     $BEERS = array("Miller Light", "\"18\" Imperial IPA", "\"The Great BOO\" Pumpkin Ale", "Wintah Ale", "Sierra Nevade, Pale Ale","Milwaukees Best",
             "Orange krush kolsch", "Vienna Lager", "Bud Light");
     // Need to start a new row after the four columns have been filled
     $counter = 0;
-    foreach($BEERS as $beer){
-        // $test = $counter%4;
-        // echo "<p>$counter</p>";
+    echo "test";
+    // var_dump(count($beers_arr)); // Returns length of list
+    foreach($beers_arr as $beer){
+        // Get variables from the object
+        $name = $beer->get_beer_name();
+        $maker = $beer->get_beer_maker();
+        $description = $beer->get_description();
+        $location = $beer->get_location();
+        $rating = $beer->get_rating();
+        $abv = $beer->get_abv();
+        $ibu = $beer->get_ibu();
+        $style = $beer->get_style();
         if ($counter % 4 === 0 || counter === 0){
             // This condition, starts a new row
             echo "
@@ -20,11 +31,15 @@
                     <div class=\"w3-quarter\">
                     <!-- This is the first card in a new row -->
                         <div class=\"w3-container w3-card w3-white w3-margin-bottom\">
-                            <h2 class=\"w3-text-grey w3-padding-16\"><i class=\"fa fa-suitcase fa-fw w3-margin-right w3-large\"></i>$beer</h2>
+                            <h2 class=\"w3-text-grey w3-padding-16\"><i class=\"fa fa-suitcase fa-fw  w3-large\"></i>$name</h2>
                             <div class=\"w3-container\">
-                                <h5 class=\"w3-opacity\"><b>Miller Lite</b></h5>
-                                <h6 class=\"w3-text-teal\"><i class=\"fa fa-calendar fa-fw w3-margin-right\"></i>10/10</span></h6>
-                                <p>Miller Lite, also known simply as Lite, is a 4.2% ABV American light pale lager sold by MillerCoors of Milwaukee, Wisconsin, United States. Miller Lite competes with Anheuser-Busch's Bud Light beer. The company also produces Miller Genuine Draft and Miller High Life.</p><br>
+                                <h5 class=\"w3-text-blue\"><b>$rating</b></h5>
+                                <h5 class=\"w3-opacity\"><b>Brewed by:</b> $maker</h5>
+                                <h5 class=\"w3-opacity\"><b>Brewed in:</b> $location</h5>
+                                <h5 class=\"w3-opacity\"><b>Brewed by:</b> $style</h5>
+                                <h5 class=\"w3-opacity\"><b>Brewed in:</b> $abv</h5>
+                                <h5 class=\"w3-opacity\"><b>Brewed in:</b> $ibu</h5>
+                                <p>$description</p><br>
                             </div>
                         </div> 
                     </div>";
@@ -35,11 +50,15 @@
                     <div class=\"w3-quarter\">
                     <!-- This is the card card # $counter -->
                         <div class=\"w3-container w3-card w3-white w3-margin-bottom\">
-                            <h2 class=\"w3-text-grey w3-padding-16\"><i class=\"fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal\"></i>$beer</h2>
+                            <h2 class=\"w3-text-grey w3-padding-16\"><i class=\"fa fa-suitcase fa-fw  w3-xxlarge w3-text-teal\"></i>$name</h2>
                             <div class=\"w3-container\">
-                                <h5 class=\"w3-opacity\"><b>$counter Miller Lite</b></h5>
-                                <h6 class=\"w3-text-teal\"><i class=\"fa fa-calendar fa-fw w3-margin-right\"></i>October 2018</span></h6>
-                                <p>Solid American Brew.</p><br>
+                                <h5 class=\"w3-text-blue\"><b>$rating</b></h5>
+                                <h5 class=\"w3-opacity\"><b>Brewed by:</b> $maker</h5>
+                                <h5 class=\"w3-opacity\"><b>Brewed in:</b> $location</h5>
+                                <h5 class=\"w3-opacity\"><b>Brewed by:</b> $style</h5>
+                                <h5 class=\"w3-opacity\"><b>Brewed in:</b> $abv</h5>
+                                <h5 class=\"w3-opacity\"><b>Brewed in:</b> $ibu</h5>
+                                <p>$description</p><br>
                             </div> 
                         </div>
                     </div>";
--- tests/beer_data.php
+++ tests/beer_data.php
@@ -1,47 +1,25 @@
 <?php
+    // Populating a list of beers for beers.php
     include("../beans/beer.php");
-    // TESTS for the Beer bean (object)
-    echo "<p><h1>Beer object test</h1></p>";
+    $beers_arr = array();
+
     $beer1 = new Beer("Blue Moon","MillerCoors","Blue Moon Belgian White (branded as Belgian Moon in Canada) is a 
     Belgian-style witbier brewed by MillerCoors under the name the Blue Moon Brewing Co. It was launched in 1995,
      and was originally brewed in Golden, Colorado.","Golden, CO","4.2/5","5.4%","80","Witbier");
-    $name = "Name: ".$beer1->get_beer_name();
-    echo "<p>$name</p>";
-
-    $maker = "Maker: ".$beer1->get_beer_maker();
-    echo "<p>$maker</p>";
-
-    $description = "Description: ".$beer1->get_description();
-    echo "<p>$description</p>";
-
-    $location = "Location: ".$beer1->get_location();
-    echo "<p>$location</p>";
-
-    $rating = "Rate: ".$beer1->get_rating();
-    echo "<p>$rating</p>";
-
-    $abv = "ABV: ".$beer1->get_abv();
-    echo "<p>$abv</p>";
-
-    $ibu = "IBU: ".$beer1->get_ibu();
-    echo "<p>$ibu</p>";
-
-    $style = "STYLE: ".$beer1->get_style();
-    echo "<p>$style</p>";
-
-    // TEST: An array of beers
-    $beers_arr = array();
     array_push($beers_arr,$beer1);
+
     $beer2 = new Beer("# 100","Nøgne Ø","Our 100th batch, brewed for the enjoyment of the brewers, but popular 
     demand forced us to release it commercially. This malty, yet light bodied ale has a massive hop bitterness. 
     Most enjoyable in a comfortable chair in front of a roaring fire.","Grimstad, Agder, Norway Lunde 8",
     "4.2/5","10%","80","Scottish-Style Light Ale");
     array_push($beers_arr,$beer2);
+
     $beer3 = new Beer("Pale Ale","Sierra Nevada Brewing Company","Our most popular beer, Sierra Nevada Pale Ale, 
     is a delightful interpretation of a classic style. It has a deep amber color and an exceptionally full-bodied, 
     complex character. ","Chico, CA",
     "4.2/5","5.6%","37","American-Style Pale Ale");
     array_push($beers_arr,$beer3);
+
     $beer4 = new Beer("Two Hearted Ale", "Bell's Brewery, Inc.", "Bell's Two Hearted Ale is defined by its intense hop aroma and malt balance. 
     Hopped exclusively with the Centennial hop varietal from the Pacific Northwest, massive additions in the kettle 
     and again in the fermenter lend their characteristic grapefruit and pine resin aromas. A significant malt body 
@@ -49,11 +27,6 @@
     remarkably drinkable American-style India Pale Ale.", "Kalamazoo, MI", "4.2/5","7%",
     "N","American-Style Indian Pale Ale");
     array_push($beers_arr,$beer4);
-    echo "<p><h1>Objects array test: </h1></p>";
-    var_dump(count($beers_arr)); // Returns length of list
-    foreach($beers_arr as $beer){
-        $loc = $beer->get_location();
-        $name = $beer->get_beer_name();
-        echo "<p>$name is brewed in $loc</p>";
-    }
+
+    // var_dump(count($beers_arr)); // Returns length of list
 ?>
