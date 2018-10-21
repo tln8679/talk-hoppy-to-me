74fbb5d65aff08afbe4c0cc9837c1b6343ac3d8f
Taylor <tlnoble94@gmail.com>
2018-10-21 15:25:36 -0400

    set up to create the cards from an array

--- beers.php
+++ beers.php
@@ -3,38 +3,53 @@
 	// Include header html here
     include('includes/header.html');
 ?>
-<div class="card-group">
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap">
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-</div>
+
+<?php
+    // Instantiating an array that we will use to populate the cards
+    $BEERS = array("Miller Light", "\"18\" Imperial IPA", "\"The Great BOO\" Pumpkin Ale", "Wintah Ale", "Sierra Nevade, Pale Ale");
+    // Need to start a new row after the four columns have been filled
+    $counter = 0;
+    foreach($BEERS as $beer){
+        // $test = $counter%4;
+        // echo "<p>$counter</p>";
+        if ($counter % 4 === 0 || counter === 0){
+            // This condition, starts a new row
+            echo "
+                <div class=\"w3-row-padding\">
+                    <div class=\"w3-quarter\">
+                    <!-- This is the first card in a new row -->
+                        <div class=\"w3-container w3-card w3-white w3-margin-bottom\">
+                            <h2 class=\"w3-text-grey w3-padding-16\"><i class=\"fa fa-suitcase fa-fw w3-margin-right w3-large\"></i>$beer</h2>
+                            <div class=\"w3-container\">
+                                <h5 class=\"w3-opacity\"><b>Miller Lite</b></h5>
+                                <h6 class=\"w3-text-teal\"><i class=\"fa fa-calendar fa-fw w3-margin-right\"></i>10/10</span></h6>
+                                <p>Miller Lite, also known simply as Lite, is a 4.2% ABV American light pale lager sold by MillerCoors of Milwaukee, Wisconsin, United States. Miller Lite competes with Anheuser-Busch's Bud Light beer. The company also produces Miller Genuine Draft and Miller High Life.</p><br>
+                            </div>
+                        </div> 
+                    </div>";
+        }
+        else{
+            // cards 2,3,4 in a row
+            echo "
+                    <div class=\"w3-quarter\">
+                    <!-- This is the card card # $counter -->
+                        <div class=\"w3-container w3-card w3-white w3-margin-bottom\">
+                            <h2 class=\"w3-text-grey w3-padding-16\"><i class=\"fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal\"></i>$beer</h2>
+                            <div class=\"w3-container\">
+                                <h5 class=\"w3-opacity\"><b>$counter Miller Lite</b></h5>
+                                <h6 class=\"w3-text-teal\"><i class=\"fa fa-calendar fa-fw w3-margin-right\"></i>October 2018</span></h6>
+                                <p>Solid American Brew.</p><br>
+                            </div> 
+                        </div>
+                    </div>";
+        }
+        if ($counter%3===0 && $counter!=0){
+            // Last card on the row, so close the row tag
+            echo "</div>";
+        }
+        $counter++;
+    }
+?>
 <?php
     include('includes/footer.html');
 ?>
