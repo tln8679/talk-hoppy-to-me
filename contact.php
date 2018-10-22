ce054e64d4f515796c3e16e34e7ec19909068faf
Taylor <tlnoble94@gmail.com>
2018-10-21 21:15:35 -0400

    added html for jacob to insert any contact info he wants

--- contact.php
+++ contact.php
@@ -7,12 +7,20 @@
         <ul>
             <li>
                 <Label> <a href=mailto:tlnoble94@gmail.com target="_blank"/>tlnoble94@gmail.com</a></Label>
-                <br/>
-                <br/>
             </li>
+            <br>
             <li>
-                <label><a href=https://www.linkedin.com/in/mrtaylornoble/>LinkedIn</a></label>
+                <label><a href=https://www.linkedin.com/in/mrtaylornoble/>LinkedIn-Taylor</a></label>
             </li>
+            <br>
+            <li>
+                <Label> <a href=#insertJacob target="_blank"/>Insert here</a></Label>
+            </li>
+            <br>
+            <li>
+                <label><a href=#insertYourLinkedIn/>LinkedIn-Jacob</a></label>
+            </li>
+            
         </ul>
     </section>
 <?php
