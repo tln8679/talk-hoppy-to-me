a6136bcc0ba6a734c9340cf516e2cc0a033a4913
Taylor <tlnoble94@gmail.com>
2018-11-07 18:52:37 -0500

    finished the beans for now

--- beans/brewery.php
+++ beans/brewery.php
@@ -1,8 +1,29 @@
 <?php 
-    class Brewery {
+    class Brewer {
+        // Added the brew id because i don't plan on adding breweries from the admin site. But we may have to.
+        private $brewerID;
+        private $name;
+        private $city;
+        private $state;
 
-    public function __construct(){
-    
-        }
+    public function __construct($brewerID,$name,$city,$state){
+        $this->brewerID=$brewerID;
+        $this->name=$name;
+        $this->avatar=$nature;
+      }
+      
+      public function getBrewerID(){
+        return $this->brewerID;
+      }
+      public function getName(){
+        return $this->name;
+      }
+      public function getCity(){
+        return $this->city;
+      }
+      public function getState(){
+        return $this->state;
+      }
+  
     }
 ?>
--- beans/post.php
+++ beans/post.php
@@ -1,9 +1,28 @@
 <?php
-//
     class Post {
+        // id is auto incremented so im not adding it because we cant have the id until we insert it into the DB
+        private $rating;
+        private $comment;
+        private $user_id;
+        private $beer_id;
 
-        public function __construct(){
-
+        public function __construct($rating,$comment,$user_id,$beer_id){
+            $this->rating=$rating;
+            $this->comment=$comment;
+            $this->user_id=$user_id;
+            $this->beer_id=$beer_id;
         }
+        public function getRating(){
+            return $this->rating;
+          }
+          public function getComment(){
+            return $this->comment;
+          }
+          public function getUserID(){
+            return $this->user_id;
+          }
+          public function getBeerID(){
+            return $this->beer_id;
+          }
     }
 ?>
