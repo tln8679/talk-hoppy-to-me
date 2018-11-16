28db237ca643ed9bfec1518e1a78331c504d1565
Taylor <tlnoble94@gmail.com>
2018-11-16 10:50:28 -0500

    finished the admin toggle in the footer

--- includes/footer.php
+++ includes/footer.php
@@ -7,14 +7,38 @@
 					<span class="glyphicon glyphicon-envelope"></span>
 					About us 
 				</a>  
-			<a href="credits.php">
+				<a href="credits.php">
 					<span class="glyphicon glyphicon-camera"></span>
 					Image credits
-			</a> 
-			<a href="suggest.php">
+				</a> 
+				<a href="suggest.php">
 					<span class="glyphicon glyphicon-comment"></span>
 					Make suggestions
-			</a> 
+				</a> 
+				<?php
+					// show admin and regular page toggles for admins
+					$dir = dirname($_SERVER['PHP_SELF']);
+					// Currently on user pages so show admin switch
+					if (isset($_SESSION['admin']) && $_SESSION['admin']==1 && !strpos($dir, 'administration')){
+				?>
+					<a href="administration/ViewUsers.php">
+						<span class="glyphicon glyphicon-queen"></span>
+						Admin pages
+					</a> 
+				<?php
+					// Close if statement
+					}
+					// User is on admin pages so show switch back to regular pages
+					if (isset($_SESSION['admin']) && $_SESSION['admin']==1 && strpos($dir, 'administration')){
+				?>
+					<a href="../index.php">
+						<span class="glyphicon glyphicon-backward"></span>
+						Back to user site
+					</a> 
+				<?php
+					// Close if statement
+					}
+				?>
 		</p>
 	</div>
 </footer>
